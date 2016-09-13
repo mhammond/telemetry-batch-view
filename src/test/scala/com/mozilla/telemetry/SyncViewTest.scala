03e6e9f47be86db31fb8188b5cbc6c10b44ada72
Mark Hammond <mhammond@skippinet.com.au>
2016-09-13 14:53:26 +1000

    write and read a parquet file

--- src/test/scala/com/mozilla/telemetry/SyncViewTest.scala
+++ src/test/scala/com/mozilla/telemetry/SyncViewTest.scala
@@ -2,7 +2,7 @@ package com.mozilla.telemetry
 
 import com.mozilla.telemetry.heka.HekaFrame
 import com.mozilla.telemetry.views.{SyncPingConverter, SyncView}
-import org.apache.spark.sql.SQLContext
+import org.apache.spark.sql.{Row, SQLContext}
 import org.apache.spark.sql.catalyst.expressions.GenericRowWithSchema
 import org.apache.spark.{SparkConf, SparkContext}
 import org.scalatest.{FlatSpec, Matchers}
@@ -112,6 +112,155 @@ class SyncViewTest extends FlatSpec with Matchers{
     }
   }
 
+  // A helper to check the contents of the multi-sync ping.
+  private def validateMultiSyncPing(rows: Array[Row]) {
+    rows.length should be (2)
+
+    val firstSync = rows(0)
+
+    firstSync.getAs[String]("app_build_id") should be ("20160907030427")
+    firstSync.getAs[String]("app_display_version") should be ("51.0a1")
+    firstSync.getAs[String]("app_name") should be ("Firefox")
+    firstSync.getAs[String]("app_version") should be ("51.0a1")
+
+    firstSync.getAs[Long]("when") should be (1473313854446L)
+    firstSync.getAs[String]("uid") should be ("12345678912345678912345678912345")
+    firstSync.getAs[Long]("took") should be (2277)
+
+    firstSync.getAs[GenericRowWithSchema]("status") should be (null)
+
+    val engines = firstSync.getAs[mutable.WrappedArray[GenericRowWithSchema]]("engines")
+    for ( engine <- engines ) {
+      val name = engine.getAs[String]("name")
+      val took = engine.getAs[Long]("took")
+      val status = engine.getAs[String]("status")
+      val failureReason = engine.getAs[GenericRowWithSchema]("failureReason")
+      val incoming = engine.getAs[GenericRowWithSchema]("incoming")
+      val outgoing = engine.getAs[mutable.WrappedArray[GenericRowWithSchema]]("outgoing")
+      name match {
+        case "clients" =>
+          took should be (468)
+          status should be (null)
+          failureReason should be (null)
+          incoming should be (null)
+          outgoing.length should be (1)
+          outgoing(0).getAs[Long]("sent") should be (1)
+
+        case "passwords" =>
+          took should be (16)
+          status should be (null)
+          failureReason should be (null)
+          incoming should be (null)
+          outgoing should be (null)
+
+        case "tabs" =>
+          took should be (795)
+          status should be (null)
+          failureReason should be (null)
+          incoming.getAs[Long]("applied") should be (2)
+          incoming.getAs[Long]("reconciled") should be (1)
+          outgoing.length should be (1)
+          outgoing(0).getAs[Long]("sent") should be (1)
+
+        case "bookmarks" =>
+          took should be (0)
+          status should be (null)
+          failureReason should be (null)
+          incoming should be (null)
+          outgoing should be (null)
+
+        case "forms" =>
+          took should be (266)
+          status should be (null)
+          failureReason should be (null)
+          incoming should be (null)
+          outgoing.length should be (1)
+          outgoing(0).getAs[Long]("sent") should be (2)
+
+        case "history" =>
+          took should be (514)
+          status should be (null)
+          failureReason should be (null)
+          incoming.getAs[Long]("applied") should be (2)
+          outgoing.length should be (1)
+          outgoing(0).getAs[Long]("sent") should be (2)
+
+        case _ =>
+          fail("Unexpected engine name")
+      }
+    }
+    // The second sync in this payload.
+    val secondSync = rows(1)
+
+    secondSync.getAs[String]("app_build_id") should be ("20160907030427")
+    secondSync.getAs[String]("app_display_version") should be ("51.0a1")
+    secondSync.getAs[String]("app_name") should be ("Firefox")
+    secondSync.getAs[String]("app_version") should be ("51.0a1")
+
+    secondSync.getAs[Long]("when") should be (1473313890947L)
+    secondSync.getAs[String]("uid") should be ("12345678912345678912345678912345")
+    secondSync.getAs[Long]("took") should be (484)
+
+    secondSync.getAs[GenericRowWithSchema]("status") should be (null)
+
+    val secondEngines = secondSync.getAs[mutable.WrappedArray[GenericRowWithSchema]]("engines")
+    for ( engine <- secondEngines ) {
+      val name = engine.getAs[String]("name")
+      val took = engine.getAs[Long]("took")
+      val status = engine.getAs[String]("status")
+      val failureReason = engine.getAs[GenericRowWithSchema]("failureReason")
+      val incoming = engine.getAs[GenericRowWithSchema]("incoming")
+      val outgoing = engine.getAs[mutable.WrappedArray[GenericRowWithSchema]]("outgoing")
+      name match {
+        case "clients" =>
+          took should be (249)
+          status should be (null)
+          failureReason should be (null)
+          incoming should be (null)
+          outgoing should be (null)
+
+        case "passwords" =>
+          took should be (0)
+          status should be (null)
+          failureReason should be (null)
+          incoming should be (null)
+          outgoing should be (null)
+
+        case "tabs" =>
+          took should be (16)
+          status should be (null)
+          failureReason should be (null)
+          incoming should be (null)
+          outgoing should be (null)
+
+        case "bookmarks" =>
+          took should be (0)
+          status should be (null)
+          failureReason should be (null)
+          incoming should be (null)
+          outgoing should be (null)
+
+        case "forms" =>
+          took should be (0)
+          status should be (null)
+          failureReason should be (null)
+          incoming should be (null)
+          outgoing should be (null)
+
+        case "history" =>
+          took should be (0)
+          status should be (null)
+          failureReason should be (null)
+          incoming should be (null)
+          outgoing should be (null)
+
+        case _ =>
+          fail("Unexpected engine name")
+      }
+    }
+
+  }
+
   "New Style SyncPing payload" can "be serialized" in {
 
     val sparkConf = new SparkConf().setAppName("SyncPing")
@@ -127,159 +276,38 @@ class SyncViewTest extends FlatSpec with Matchers{
 
       dataframe.show() // See the console here
 
-      // verify the contents - there are 2 syncs in this single ping.
-      val rows = dataframe.collect()
-      rows.length should be (2)
-
-      val firstSync = rows(0)
-
-      firstSync.getAs[String]("app_build_id") should be ("20160907030427")
-      firstSync.getAs[String]("app_display_version") should be ("51.0a1")
-      firstSync.getAs[String]("app_name") should be ("Firefox")
-      firstSync.getAs[String]("app_version") should be ("51.0a1")
-
-      firstSync.getAs[Long]("when") should be (1473313854446L)
-      firstSync.getAs[String]("uid") should be ("12345678912345678912345678912345")
-      firstSync.getAs[Long]("took") should be (2277)
-
-      firstSync.getAs[GenericRowWithSchema]("status") should be (null)
-
-      val engines = firstSync.getAs[mutable.WrappedArray[GenericRowWithSchema]]("engines")
-      for ( engine <- engines ) {
-        val name = engine.getAs[String]("name")
-        val took = engine.getAs[Long]("took")
-        val status = engine.getAs[String]("status")
-        val failureReason = engine.getAs[GenericRowWithSchema]("failureReason")
-        val incoming = engine.getAs[GenericRowWithSchema]("incoming")
-        val outgoing = engine.getAs[mutable.WrappedArray[GenericRowWithSchema]]("outgoing")
-        name match {
-          case "clients" =>
-            took should be (468)
-            status should be (null)
-            failureReason should be (null)
-            incoming should be (null)
-            outgoing.length should be (1)
-            outgoing(0).getAs[Long]("sent") should be (1)
-
-          case "passwords" =>
-            took should be (16)
-            status should be (null)
-            failureReason should be (null)
-            incoming should be (null)
-            outgoing should be (null)
-
-          case "tabs" =>
-            took should be (795)
-            status should be (null)
-            failureReason should be (null)
-            incoming.getAs[Long]("applied") should be (2)
-            incoming.getAs[Long]("reconciled") should be (1)
-            outgoing.length should be (1)
-            outgoing(0).getAs[Long]("sent") should be (1)
-
-          case "bookmarks" =>
-            took should be (0)
-            status should be (null)
-            failureReason should be (null)
-            incoming should be (null)
-            outgoing should be (null)
-
-          case "forms" =>
-            took should be (266)
-            status should be (null)
-            failureReason should be (null)
-            incoming should be (null)
-            outgoing.length should be (1)
-            outgoing(0).getAs[Long]("sent") should be (2)
-
-          case "history" =>
-            took should be (514)
-            status should be (null)
-            failureReason should be (null)
-            incoming.getAs[Long]("applied") should be (2)
-            outgoing.length should be (1)
-            outgoing(0).getAs[Long]("sent") should be (2)
-
-          case _ =>
-            fail("Unexpected engine name")
-        }
-      }
-      // The second sync in this payload.
-      val secondSync = rows(1)
-
-      secondSync.getAs[String]("app_build_id") should be ("20160907030427")
-      secondSync.getAs[String]("app_display_version") should be ("51.0a1")
-      secondSync.getAs[String]("app_name") should be ("Firefox")
-      secondSync.getAs[String]("app_version") should be ("51.0a1")
-
-      secondSync.getAs[Long]("when") should be (1473313890947L)
-      secondSync.getAs[String]("uid") should be ("12345678912345678912345678912345")
-      secondSync.getAs[Long]("took") should be (484)
-
-      secondSync.getAs[GenericRowWithSchema]("status") should be (null)
-
-      val secondEngines = secondSync.getAs[mutable.WrappedArray[GenericRowWithSchema]]("engines")
-      for ( engine <- secondEngines ) {
-        val name = engine.getAs[String]("name")
-        val took = engine.getAs[Long]("took")
-        val status = engine.getAs[String]("status")
-        val failureReason = engine.getAs[GenericRowWithSchema]("failureReason")
-        val incoming = engine.getAs[GenericRowWithSchema]("incoming")
-        val outgoing = engine.getAs[mutable.WrappedArray[GenericRowWithSchema]]("outgoing")
-        name match {
-          case "clients" =>
-            took should be (249)
-            status should be (null)
-            failureReason should be (null)
-            incoming should be (null)
-            outgoing should be (null)
-
-          case "passwords" =>
-            took should be (0)
-            status should be (null)
-            failureReason should be (null)
-            incoming should be (null)
-            outgoing should be (null)
-
-          case "tabs" =>
-            took should be (16)
-            status should be (null)
-            failureReason should be (null)
-            incoming should be (null)
-            outgoing should be (null)
-
-          case "bookmarks" =>
-            took should be (0)
-            status should be (null)
-            failureReason should be (null)
-            incoming should be (null)
-            outgoing should be (null)
-
-          case "forms" =>
-            took should be (0)
-            status should be (null)
-            failureReason should be (null)
-            incoming should be (null)
-            outgoing should be (null)
-
-          case "history" =>
-            took should be (0)
-            status should be (null)
-            failureReason should be (null)
-            incoming should be (null)
-            outgoing should be (null)
-
-          case _ =>
-            fail("Unexpected engine name")
-        }
-      }
-
+      // verify the contents
+      validateMultiSyncPing(dataframe.collect())
     } finally {
       sc.stop()
     }
   }
 
-  "SyncPing records" can "be serialized" in {
+  "SyncPing records" can "be round-tripped to parquet" in {
+    val sparkConf = new SparkConf().setAppName("SyncPing")
+    sparkConf.setMaster(sparkConf.get("spark.master", "local[1]"))
+    val sc = new SparkContext(sparkConf)
+    sc.setLogLevel("WARN")
+    try {
+      val row = SyncPingConverter.pingToRows(SyncViewTestPayloads.multiSyncPing)
+      // Write a parquet file with the rows.
+      val sqlContext = new SQLContext(sc)
+      val rdd = sc.parallelize(row.toSeq)
+      val dataframe = sqlContext.createDataFrame(rdd, SyncPingConverter.syncType)
+      val tempFile = com.mozilla.telemetry.utils.temporaryFileName()
+      dataframe.write.parquet(tempFile.toString)
+      // read it back in and verify it.
+      val localDataset = sqlContext.read.load(tempFile.toString)
+      localDataset.registerTempTable("sync")
+      val localDataframe = sqlContext.sql("SELECT * FROM sync")
+      localDataframe.show() // See the console here
+      validateMultiSyncPing(localDataframe.collect())
+    } finally {
+      sc.stop()
+    }
+  }
+
+  "SyncPing records" can "be read from heka files" in {
     val sparkConf = new SparkConf().setAppName("SyncPing")
     sparkConf.setMaster(sparkConf.get("spark.master", "local[1]"))
     val sc = new SparkContext(sparkConf)
@@ -289,7 +317,6 @@ class SyncViewTest extends FlatSpec with Matchers{
       // telemetry-2/20160831/telemetry/4/sync/Firefox/nightly/51.0a1/20160830030201/
       val hekaFileName = "/sync-ping.heka"
       val hekaURL = getClass.getResource(hekaFileName)
-      println(s" URL = '${hekaURL}'")
       val input = hekaURL.openStream()
       val rows = HekaFrame.parse(input).flatMap(SyncView.messageToRow)
 
